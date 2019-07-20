d037d63a2182988537c66395a3f6057a77d16e4f
lizKimita <kimita.wanjiku@gmail.com>
2019-07-20 03:18:13 +0300

    adding api and data request feature

--- table/views.py
+++ table/views.py
@@ -1,8 +1,20 @@
+import requests
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
     title = "Restaurant table"
+    url = 'https://api.airtable.com/v0/apptf9xPJazK1Lso8/Transactions?api_key=keyBuxrgTLexTtnqU'
+    
+    transactions = requests.get(url.format()).json()
+   
+
+    restaurant_table = {
+        "name":transactions['Name'],
+
+    }
+    print(restaurant_table)
 
     return render(request,'home.html', {"title":title})
     
