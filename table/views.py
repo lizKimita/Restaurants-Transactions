a2e207dd9139bffdeedcd916a89ca4431392cfd3
lizKimita <kimita.wanjiku@gmail.com>
2019-07-20 21:30:43 +0300

    changing the views file

--- table/views.py
+++ table/views.py
@@ -1,16 +1,2 @@
-import requests
-from django.shortcuts import render
-import datetime
 
-
-# Create your views here.
-def home(request):
-    title = "Restaurant table"
-    url = 'https://api.airtable.com/v0/apptf9xPJazK1Lso8/Transactions?api_key=keyBuxrgTLexTtnqU'
-    
-    transactions = requests.get(url.format()).json()
-
-    table = transactions["records"]
-   
-    return render(request,'home.html',{'title':title, 'table':table})
     
