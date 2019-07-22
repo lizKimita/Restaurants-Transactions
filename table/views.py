807e8a4425c6df594c1801f7cbc85feaae4ff8e9
lizKimita <kimita.wanjiku@gmail.com>
2019-07-22 14:42:19 +0300

    changing time format function in the views file and displaying the correct time in the template file

--- table/views.py
+++ table/views.py
@@ -11,23 +11,20 @@ def home(request):
     transactions = requests.get(url.format()).json()
 
     table = transactions["records"]
-    date = [];
 
     for i in table:
-        time =i["fields"]["Timestamp"]
-        newtime = datetime.datetime.strptime(time,"%Y-%m-%dT%H:%M:%S.000Z").strftime('%d-%m-%Y')
-        date.append(newtime)
+        i["fields"]["Timestamp"]= datetime.datetime.strptime(i["fields"]["Timestamp"],'%Y-%m-%dT%H:%M:%S.000Z').strftime('%d-%m-%Y')
 
 
-    return render(request,'home.html',{'title':title, 'table':table, 'date':date})
+    return render(request,'home.html',{'title':title, 'table':table})
     
 
-    # branch = i["fields"]["Branch"]
-    # amount = i["fields"]["Amount"]
-    # appended = "**Top in {branch}**"
+    branch = i["fields"]["Branch"]
+    amount = i["fields"]["Amount"]
+    appended = "**Top in {branch}**"
 
-    # for n in range (len(branch)):
-    # if branch[n] == branch[n+1] and amount[n] > amount[n+1] or amount[n+1] > amount[n]:
-    #     print (branch)
+    for n in range (len(branch)):
+        if branch[n] == branch[n+1] and amount[n] > amount[n+1] or amount[n+1] > amount[n]:
+            print (branch)
 
-    # return render(request,'home.html')
+    return render(request,'home.html')
