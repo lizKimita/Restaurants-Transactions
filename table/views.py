cfeb092dd2f09ad816d4785b09ca69e20b304790
lizKimita <kimita.wanjiku@gmail.com>
2019-07-22 16:34:42 +0300

    adding the top spenders filter in the view and displaying them on the html file

--- table/views.py
+++ table/views.py
@@ -12,19 +12,33 @@ def home(request):
 
     table = transactions["records"]
 
+    # to convert the time format
     for i in table:
         i["fields"]["Timestamp"]= datetime.datetime.strptime(i["fields"]["Timestamp"],'%Y-%m-%dT%H:%M:%S.000Z').strftime('%d-%m-%Y')
 
+    branches_dict={}
+
+    for i in table:
+        # removes branch duplicates and stores them as dictionaries 
+        if i["fields"]["Branch"] not in branches_dict:
+            branches_dict[i["fields"]["Branch"]]=[]
+
+        # to have all the the amounts in their respective branches
+        if i["fields"]["Branch"] in branches_dict:
+            for j in branches_dict:
+                if j==i["fields"]["Branch"]:
+                    branches_dict[j].append(i["fields"]["Amount"])  
+
+    # to get the highest amount per branch
+    for j in branches_dict:
+        branches_dict[j]=max(branches_dict[j])
+    
+        # to check the highest spender per branch and highlight them
+        for i in table:
+            if i["fields"]["Amount"]== branches_dict[j]:
+                i["fields"]["high"]= f"Top in {i['fields']['Branch']}"
 
     return render(request,'home.html',{'title':title, 'table':table})
     
 
-    branch = i["fields"]["Branch"]
-    amount = i["fields"]["Amount"]
-    appended = "**Top in {branch}**"
-
-    for n in range (len(branch)):
-        if branch[n] == branch[n+1] and amount[n] > amount[n+1] or amount[n+1] > amount[n]:
-            print (branch)
-
-    return render(request,'home.html')
+  
